637ae5911b9f009de8bc0a45326d306b8a5e0963
Anthony Legere <anthonyclegere@gmail.com>
2022-11-25 10:45:58 -0500

    displaying weather data at this time, need to add more styling and catch errors

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,21 @@
 /******/ (() => { // webpackBootstrap
 var __webpack_exports__ = {};
-async function getWeatherMain(location){
+const currentTemp = document.querySelector("#currentTemp");
+const feelsLike = document.querySelector("#feelsLike");
+const todayHi = document.querySelector("#tempMax");
+const todayLow = document.querySelector("#tempMin");
+const windSpeed = document.querySelector("#wind");
 
+async function getWeatherMain(location){
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location},US&units=imperial&appid=9f75ca6643a67f1e2530e0c39ee7627d`, {mode: 'cors'});
     const weatherStuff = await response.json();
     console.log(weatherStuff);
+
+    currentTemp.innerHTML = Math.round(weatherStuff.main.temp);
+    feelsLike.innerHTML = Math.round(weatherStuff.main.feels_like);
+    todayHi.innerHTML = Math.round(weatherStuff.main.temp_max);
+    todayLow.innerHTML = Math.round(weatherStuff.main.temp_min);
+    windSpeed.innerHTML = Math.round(weatherStuff.wind.speed) + ` MPH`;    
 }
 
 const submit = document.querySelector("#submit");
@@ -13,11 +24,5 @@ submit.addEventListener("click", (e)=>{
     let loc = document.querySelector("#loc").value;
     getWeatherMain(loc);
 })
-
-
-
- 
-
-
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-async function getWeatherMain(location){
+const currentTemp = document.querySelector("#currentTemp");
+const feelsLike = document.querySelector("#feelsLike");
+const todayHi = document.querySelector("#tempMax");
+const todayLow = document.querySelector("#tempMin");
+const windSpeed = document.querySelector("#wind");
 
+async function getWeatherMain(location){
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location},US&units=imperial&appid=9f75ca6643a67f1e2530e0c39ee7627d`, {mode: 'cors'});
     const weatherStuff = await response.json();
     console.log(weatherStuff);
+
+    currentTemp.innerHTML = Math.round(weatherStuff.main.temp);
+    feelsLike.innerHTML = Math.round(weatherStuff.main.feels_like);
+    todayHi.innerHTML = Math.round(weatherStuff.main.temp_max);
+    todayLow.innerHTML = Math.round(weatherStuff.main.temp_min);
+    windSpeed.innerHTML = Math.round(weatherStuff.wind.speed) + ` MPH`;    
 }
 
 const submit = document.querySelector("#submit");
@@ -11,8 +22,3 @@ submit.addEventListener("click", (e)=>{
     let loc = document.querySelector("#loc").value;
     getWeatherMain(loc);
 })
-
-
-
- 
-
